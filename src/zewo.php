6b7be4fab28adf0d98c3ddd1589e1afbe05d4bb0
Pierre-Antoine Delnatte <info@flatland.be>
2012-03-09 22:31:53 +0100

    Loading classes

--- src/zewo.php
+++ src/zewo.php
@@ -6,5 +6,16 @@
 namespace Zewo;
 
 // TODO : loading all classes
+include( __DIR__ . '/tools/functions.php' );
+include( __DIR__ . '/tools/singleton.php' );
+
+include( __DIR__ . '/classes/routing/router.php' );
+include( __DIR__ . '/classes/routing/route.php' );
+
+include( __DIR__ . '/classes/zewo.php' );
+
 // TODO : init shits and stuffs
+static $zewo;
+$zewo = Zewo::getInstance();
+
 // TODO : init static $zewo variable
