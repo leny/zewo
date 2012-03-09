07ff3dfce2077dd797b2a581fd81daf7fac11d14
Pierre-Antoine Delnatte <info@flatland.be>
2012-03-09 22:31:44 +0100

    Routing and Shortcuts

---
+++ src/classes/routing/route.php
@@ -0,0 +1,51 @@
+<?php
+/** flatLand! : zewo
+ * /classes/routing/route.php : main routing classe
+ */
+
+namespace Zewo\Routing;
+
+class Route {
+
+	public function __construct( $sPattern, $aMethods, $aCallbacks, $bIsAJAX = false ) {
+		$this->_sPattern = $sPattern;
+		$this->_aAllowedMethods = $aMethods;
+		foreach( $aCallbacks as $cCallback ) {
+			if( !is_callable( $cCallback ) )
+				throw new InvalidArgumentException( "A route middleware must be callable !" );
+			$this->_aCallbacks[] = $cCallback;
+		}
+		$this->_bIsAJAX = $bIsAJAX;
+	} // __construct
+
+	public function match( $sURI ) {
+		if( $this->_isMatching( $sURI ) ) {
+			$this->_sCurrentURI = $sURI;
+			return true;
+		} else return false;
+	} // match
+
+	public function exec() {
+		foreach( $this->_aCallbacks as $cCallback )
+			call_user_func_array( $cCallback, array() ); // TODO : add params
+	} // exec
+
+	private function _isMatching( $sURI ) {
+		$bMatching = true;
+		// matching method
+		$bMatching = $bMatching && ( in_array( strtolower( $_SERVER[ 'REQUEST_METHOD' ] ), $this->_aAllowedMethods ) );
+		// TODO : matching ajax if needed
+		// TODO : matching params
+		// matching pattern
+		$bMatching = $bMatching && ( $sURI == $this->_sPattern );
+		return $bMatching;
+	} // _isMatching
+
+	private $_sCurrentURI;
+
+	private $_bIsAJAX = false;
+	private $_sPattern;
+	private $_aAllowedMethods;
+	private $_aCallbacks = array();
+
+} // class::Route
---
+++ src/classes/zewo.php
@@ -0,0 +1,51 @@
+<?php
+/** flatLand! : zewo
+ * /classes/zewo.php : main classe
+ */
+
+namespace Zewo;
+
+class Zewo extends Tools\Singleton {
+
+	public function __get( $sName ) {
+		switch( $sName ) {
+			case 'route':
+				return $this->_oRouting;
+				break;
+			// TODO : case 'template': break;
+			// TODO : case 'utils': break;
+			// TODO : case 'db': break;
+		}
+	} // __get
+
+	// shortcuts
+
+	public function __call( $sName, $aArguments ) {
+		switch( $sName ) {
+			// ROUTING shortcuts
+			case 'run':
+			case 'post':
+			case 'get':
+			case 'map':
+			case 'error':
+			case 'redirect':
+				call_user_func_array( array( $this->route, $sName ), $aArguments );
+				break;
+		}
+	} // __call
+
+	public function init( $mConfig=null ) {
+		// TODO : check config type
+		$this->_applyConfig();
+	} // init
+
+	private function _applyConfig() {
+		$this->_oRouting = Routing\Router::getInstance();
+	} // _applyConfig
+
+	private $_oTemplate;
+	private $_oRouting;
+	private $_oUtils;
+	private $_oDB;
+
+} // class::Zewo
