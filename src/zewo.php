45219ad8e5fb273cd4dcc148ac5712adc4eb3f09
Pierre-Antoine Delnatte <info@flatland.be>
2012-03-09 19:51:39 +0100

    Defining namespace

---
+++ src/zewo.php
@@ -0,0 +1,10 @@
+<?php
+/** flatLand! : zewo
+ * /zewo.php : main entry point
+ */
+
+namespace Zewo;
+
+// TODO : loading all classes
+// TODO : init shits and stuffs
+// TODO : init static $zewo variable
