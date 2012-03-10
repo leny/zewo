d70cf3c8d54e17ae3ecc053d6a19893f003e8696
Leny McCormick <lisarael@gmail.com>
2012-03-10 15:02:21 +0100

    Refactoring Config

--- src/classes/config/config.php
+++ src/classes/config/config.php
@@ -5,62 +5,36 @@
 
 namespace Zewo\Config;
 
-// TODO : refactor this
-
 class Config extends \Zewo\Tools\Singleton {
 
-	public function __get( $sName ) {
-		switch( $sName ) {
-			case 'db_connexion':
-				return $this->_sDBConnexion;
-				break;
-			case 'db_host':
-				return $this->_sDBHost;
-				break;
-			case 'db_login':
-				return $this->_sDBLogin;
-				break;
-			case 'db_pass':
-				return $this->_sDBPass;
-				break;
-			case 'db_base':
-				return $this->_sDBBase;
-				break;
-			default:
-				return parent::__get( $sName );
-				break;
+	public function get( $sConfigPath ) {
+		$aCurrent = $this->_aData;
+		foreach( explode( '.', $sConfigPath ) as $sConfigPathPart ) {
+			if( is_array( $aCurrent ) )
+				$aCurrent = isset( $aCurrent[ $sConfigPathPart ] ) ? $aCurrent[ $sConfigPathPart ] : null;
 		}
-	} // __get
+		return $aCurrent;
+	} // get
 
 	public function apply( $aConfig ) {
-		$this->_apply( $aConfig );
+		$this->_aData = array_merge( $this->_aDefault, $aConfig );
 	} // apply
 
-	private function _apply( $aConfig ) {
-		// db
-		if( isset( $aConfig[ 'db' ] ) ) {
-			if( isset( $aConfig[ 'db' ][ 'connexion' ] ) )
-				$this->_sDBConnexion = $aConfig[ 'db' ][ 'connexion' ];
-			if( isset( $aConfig[ 'db' ][ 'host' ] ) )
-				$this->_sDBHost = $aConfig[ 'db' ][ 'host' ];
-			if( isset( $aConfig[ 'db' ][ 'login' ] ) )
-				$this->_sDBLogin = $aConfig[ 'db' ][ 'login' ];
-			if( isset( $aConfig[ 'db' ][ 'pass' ] ) )
-				$this->_sDBPass = $aConfig[ 'db' ][ 'pass' ];
-			if( isset( $aConfig[ 'db' ][ 'base' ] ) )
-				$this->_sDBBase = $aConfig[ 'db' ][ 'base' ];
-		}
-	} // _apply
-
-	// DB
-	private $_sDBConnexion = '';
-	private $_sDBHost = 'localhost';
-	private $_sDBLogin = '';
-	private $_sDBPass = '';
-	private $_sDBBase = '';
-
-	// ORM
-	private $_sCacheKey = 'noCacheKey';
-	private $_sBaseClass = 'Zewo\ORM\Element';
+	private $_aData = array();
+	private $_aDefault = array(
+		// DB
+		'db' => array(
+			'connexion' => '',
+			'host' => 'localhost',
+			'login' => '',
+			'pass' => '',
+			'base' => '',
+		),
+		// ORM
+		'orm' => array(
+			'cacheKey' => 'noCacheKey',
+			'baseClass' => 'Zewo\ORM\ZewoElement'
+		),
+	);
 
 } // class::Config
