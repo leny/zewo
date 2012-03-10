32c2bc3715dc439f2a4bb05f161cc780179b196e
Pierre-Antoine Delnatte <info@flatland.be>
2012-03-10 12:10:45 +0100

    Routing : routes de base

--- src/classes/routing/route.php
+++ src/classes/routing/route.php
@@ -5,16 +5,10 @@
 
 namespace Zewo\Routing;
 
-// TODO : matching params in route
-// TODO : passing params to callbacks
-// TODO : matching optional params in route
-// TODO : matching * in route
-// TODO : matching AJAX
-
 class Route {
 
 	public function __construct( $sPattern, $aMethods, $aCallbacks, $bIsAJAX = false ) {
-		$this->_sPattern = $sPattern;
+		$this->_sPattern = str_replace( ')', ')?', $sPattern );
 		$this->_aAllowedMethods = $aMethods;
 		foreach( $aCallbacks as $cCallback ) {
 			if( !is_callable( $cCallback ) )
@@ -22,10 +16,11 @@ class Route {
 			$this->_aCallbacks[] = $cCallback;
 		}
 		$this->_bIsAJAX = $bIsAJAX;
+		$this->_generateRegex();
 	} // __construct
 
 	public function match( $sURI ) {
-		if( $this->_isMatching( $sURI ) ) {
+		if( $this->_match( $sURI ) ) {
 			$this->_sCurrentURI = $sURI;
 			return true;
 		} else return false;
@@ -33,25 +28,71 @@ class Route {
 
 	public function exec() {
 		foreach( $this->_aCallbacks as $cCallback )
-			call_user_func_array( $cCallback, array() ); // TODO : add params
+			call_user_func_array( $cCallback, array_values( $this->_aParams ) );
 	} // exec
 
-	private function _isMatching( $sURI ) {
+	private function _match( $sURI ) {
 		$bMatching = true;
-		// matching method
-		$bMatching = $bMatching && ( in_array( strtolower( $_SERVER[ 'REQUEST_METHOD' ] ), $this->_aAllowedMethods ) );
-		// TODO : matching ajax if needed
-		// TODO : matching params
-		// matching pattern
-		$bMatching = $bMatching && ( $sURI == $this->_sPattern );
+		$bMatching = $bMatching && $this->_matchMethod();
+		$bMatching = $bMatching && $this->_matchAJAX();
+		$bMatching = $bMatching && $this->_matchURL( $sURI );
 		return $bMatching;
-	} // _isMatching
+	} // _match
+
+	private function _matchMethod() {
+		return in_array( strtolower( $_SERVER[ 'REQUEST_METHOD' ] ), $this->_aAllowedMethods );
+	} // _matchMethod
+
+	private function _matchAJAX() {
+		return $this->_bIsAJAX ? ( strtolower( $_SERVER[ 'HTTP_X_REQUESTED_WITH' ] ) == 'xmlhttprequest' ) : true;
+	} // _matchAJAX
+
+	private function _matchURL( $sURI ) {
+		if ( preg_match( $this->_sPatternAsRegex, $sURI, $aParamValues ) ) {
+			array_shift( $aParamValues );
+			foreach ( $this->_aParamNames as $i => $sValue ) {
+				$sVal = substr( $sValue, 1 );
+				if ( isset( $aParamValues[ $sVal ] ) ) {
+					$this->_aParams[ $sVal ] = urldecode( $aParamValues[ $sVal ] );
+				}
+			}
+			return true;
+		} else
+			return false;
+	} // _matchURL
+
+	private function _generateRegex() {
+		// extract url params
+		preg_match_all( '@:([\w]+)@', $this->_sPattern, $aParamNames, PREG_PATTERN_ORDER );
+		$this->_aParamNames = $aParamNames[0];
+
+		// convert url params to regex
+		$sPatternAsRegex = preg_replace_callback( '@:[\w]+@', array( $this, '_convertPatternToRegex' ), $this->_sPattern );
+		if ( substr($this->_sPattern, -1) === '/' ) {
+			$sPatternAsRegex = $sPatternAsRegex . '?';
+		}
+		$this->_sPatternAsRegex = '@^' . $sPatternAsRegex . '$@';
+	} // _generateRegex
+
+	private function _convertPatternToRegex( $aMatches ) {
+		$sKey = str_replace(':', '', $aMatches[0]);
+		if ( array_key_exists( $sKey, $this->_aConditions ) )
+			return '(?P<' . $sKey . '>' . $this->_aConditions[$sKey] . ')';
+		else
+			return '(?P<' . $sKey . '>[a-zA-Z0-9_\-\.\!\~\*\\\'\(\)\:\@\&\=\$\+,%]+)';
+	} // _convertPatternToRegex
 
 	private $_sCurrentURI;
 
 	private $_bIsAJAX = false;
 	private $_sPattern;
 	private $_aAllowedMethods;
+	private $_aConditions = array();
 	private $_aCallbacks = array();
+
+	private $_sPatternAsRegex;
+
+	private $_aParams = array();
+	private $_aParamNames = array();
 
 } // class::Route
