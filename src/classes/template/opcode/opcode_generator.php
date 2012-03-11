14660c9d57761bd2052a1b13c5db52a69bebb77c
Leny McCormick <lisarael@gmail.com>
2012-03-11 12:57:21 +0100

    Templating : generate opcode : constantes

--- src/classes/template/opcode/opcode_generator.php
+++ src/classes/template/opcode/opcode_generator.php
@@ -16,6 +16,7 @@ class OpcodeGenerator {
 		$this->_initOpcode( $sOpcodeID );
 		$this->_replaceComments();
 		$this->_replaceVars();
+		$this->_replaceConstants();
 
 		return $this->_sOpcodeReturn;
 	} // generate
@@ -37,7 +38,6 @@ class OpcodeGenerator {
 	} // _replaceVars
 
 	private function _parseVar( $aMatches ) {
-		// building var expression
 		$aVarComponenents = preg_split( '/(\.|\-\>|\[.+\])/', $aMatches[1], -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE );
 		$sVarName = '';
 		for( $i = -1, $l = sizeof( $aVarComponenents ); ++$i < $l; ) {
@@ -48,17 +48,31 @@ class OpcodeGenerator {
 			else
 				$sVarName .= $aVarComponenents[ $i ];
 		}
-		// functions applied
-		if( isset( $aMatches[2] ) ) {
-			$aFunctions = explode( '|', $aMatches[2] );
-			foreach( $aFunctions as $sFunction ) {
-				$aFunctionComponents = explode( ':', $sFunction );
-				$sFunctionName = array_shift( $aFunctionComponents );
-				$sVarName = $sFunctionName . '( ' . $sVarName .  '' . ( sizeof( $aFunctionComponents ) ? ', ' . implode( ', ', $aFunctionComponents ) : '' ) . ' )';
-			}
-		}
+		if( isset( $aMatches[ 2 ] ) )
+			$sVarName = $this->_applyFunctionToVarExpression( $sVarName, $aMatches[ 2 ] );
 		return '<?=' . $sVarName . '; ?>';
 	} // _parseVar
+
+	private function _replaceConstants() {
+		$this->_sOpcodeReturn = preg_replace_callback( $this->_sConstantsRegex, array( $this, '_parseConstants' ), $this->_sOpcodeReturn );
+	} // _replaceConstants
+
+	private function _parseConstants( $aMatches ) {
+		$sVarName = $aMatches[ 1 ];
+		if( isset( $aMatches[ 2 ] ) )
+			$sVarName = $this->_applyFunctionToVarExpression( $sVarName, $aMatches[ 2 ] );
+		return '<?=' . $sVarName . '; ?>';
+	} // _parseConstants
+
+	private function _applyFunctionToVarExpression( $sVarName, $sFunctionsDefinition ) {
+		$aFunctions = explode( '|', $sFunctionsDefinition );
+		foreach( $aFunctions as $sFunction ) {
+			$aFunctionComponents = explode( ':', $sFunction );
+			$sFunctionName = array_shift( $aFunctionComponents );
+			$sVarName = $sFunctionName . '( ' . $sVarName .  '' . ( sizeof( $aFunctionComponents ) ? ', ' . implode( ', ', $aFunctionComponents ) : '' ) . ' )';
+		}
+		return $sVarName;
+	} // _applyFunctionToVarExpression
 
 	private $_sTemplateSource;
 	private $_sOpcodeReturn;
@@ -69,5 +83,6 @@ class OpcodeGenerator {
 	private $_sSimpleCommentsRegex = '/(\{\*.+\*\})/';
 	private $_sBlockCommentsRegex = '/(\{\*\}.+\{\*\})/sme';
 	private $_sVarsRegex = '/\{(\$[^\|\}]+)[\|]*(.+)*\}/';
+	private $_sConstantsRegex = '/\{#([^\|\}]+)[\|]*(.+)*\}/';
 
 } // class::OpcodeGenerator
