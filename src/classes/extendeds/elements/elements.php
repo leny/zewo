375446c6851a145ed6e6c32b96177e6710c89649
Pierre-Antoine Delnatte <info@flatland.be>
2012-03-17 03:08:45 +0100

    Extendeds for Elements and Element

---
+++ src/classes/extendeds/elements/elements.php
@@ -0,0 +1,19 @@
+<?php
+/** flatLand! : zewo
+ * /classes/extendeds/elements/element.php
+ */
+
+namespace \Zewo\Extendeds\Elements;
+
+abstract class Elements extends \Zewo\ORM\Elements\Elements {
+
+	public function __construct( $sTargetClass, $sQuery ) {
+		return parent::__construct( $sTargetClass, $sQuery );
+	} // __construct
+
+	public function assign( $sName ) {
+		\Zewo\Zewo::getInstance()->tpl->assignByRef( $sName, $this );
+		return $this;
+	} // assign
+
+} // class::Elements
