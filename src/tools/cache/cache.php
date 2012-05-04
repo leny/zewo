dac5fe567be7606b178fbe605650acfbdbfca97d
Pierre-Antoine Delnatte <info@flatland.be>
2012-05-04 15:17:12 +0200

    Ajout d'un nouveau type de cache : SESSION

--- src/tools/cache/cache.php
+++ src/tools/cache/cache.php
@@ -8,11 +8,15 @@ namespace Zewo\Tools\Cache;
 abstract class Cache {
 
 	const APC = 'APC';
+	const SESSION = 'SESSION';
 
 	public static function getInstanceOf( $sType ) {
 		switch( $sType ) {
 			case self::APC:
 				return \Zewo\Tools\Cache\APC::getInstance();
+				break;
+			case self::SESSION:
+				return \Zewo\Tools\Cache\SessionCache::getInstance();
 				break;
 			default:
 				throw new \InvalidArgumentException( 'Unknown Cache Type "' . $sType . '" !' );
